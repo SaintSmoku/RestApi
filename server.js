2010732b745bf483d058261e076c1364bcc551d3
SaintSmok <saintsmoku@gmail.com>
2025-09-07 22:18:54 +0800

    Second commit

--- server.js
+++ server.js
@@ -6,8 +6,44 @@ dotenv.config( { path: "./config/config.env" } );
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api/v1/categories", (req, res) => {
+  // res.send("Hello World");
+  res.status(200).json({
+    success : true,
+    data : "All categories are here"
+  })
+});
+
+app.post("/api/v1/categories", (req, res) => {
+  // res.send("Hello World");
+  res.status(200).json({
+    success : true,
+    data : "Create new category..."
+  })
+});
+
+app.put("/api/v1/categories/:id", (req, res) => {
+  // res.send("Hello World");
+  res.status(200).json({
+    success : true,
+    data : `Change the category on id ${req.params.id}`
+  })
+});
+
+app.delete("/api/v1/categories/:id", (req, res) => {
+  // res.send("Hello World");
+  res.status(200).json({
+    success : true,
+    data : `Delete the category id ${req.params.id}`
+  })
+});
+
+app.get("/api/v1/categories/:id", (req, res) => {
+  // res.send("Hello World");
+  res.status(200).json({
+    success : true,
+    data : `Get the category id ${req.params.id}`
+  })
 });
 
 
